2eff678eb249421a58a3cf9deb616992f8d6eaa6
ptoil <suhailmallick@gmail.com>
2022-09-21 16:38:26 -0400

    manual readMemory and main method to test

--- src/Memory.java
+++ src/Memory.java
@@ -5,21 +5,49 @@ public class Memory {
 	private char[] memArray;
 	private final char MEMORY_SIZE = 200;
 	
-	public Memory() {
-		memArray = new char[MEMORY_SIZE];
+	public Memory(int size) {
+//		memArray = new char[MEMORY_SIZE];
+		memArray = new char[size];
+		initializeMemory();
 	}
 	
-	public char[] readMemory(int address, int size) {
-		return Arrays.copyOfRange(memArray, address, address+size);
-	}
-	
-	public void writeMemory(int address, char[] buffer) {
-		for (int i = 0; i < buffer.length; i++) {
-			memArray[address + i] = buffer[i];
+	private void initializeMemory() {
+		for (int i = 0; i < memArray.length; i++) {
+			memArray[i] = '0';
 		}
 	}
 	
-	public int getIndex(char[] buffer) {
-		return 0;
+	public char[] readMemory(int address, int size) {
+//		return Arrays.copyOfRange(memArray, address, address+size);
+		char[] a = new char[size];
+		for (int i = 0; i < size; i++) {
+			a[i] = memArray[address + i];
+		}
+		return a;
+	}
+	
+	public char[] getMemArray() {
+		return memArray;
+	}
+	
+	public void writeMemory(int address, String buffer) {
+		for (int i = 0; i < buffer.length(); i++) {
+			memArray[address + i] = buffer.charAt(i);
+		}
+	}
+	
+	public int getIndex(String buffer) {
+		return 0; //temp
+	}
+	
+	public static void main(String[] args) {
+		Memory RAM = new Memory(10);
+		System.out.println("test");
+		System.out.println(RAM.getMemArray());
+		RAM.writeMemory(2, "hi");
+		System.out.println(RAM.getMemArray());
+		RAM.writeMemory(3, "yo");
+		System.out.println(RAM.getMemArray());
+		System.out.println(RAM.readMemory(2, 3));
 	}
 }
